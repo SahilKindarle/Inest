024aa011c79bc3607b4f35f62a87bb5472103f19
Krushn Dayshmookh <krushn.dayshmookh.ext@siemens.com>
2024-03-27 21:20:08 +0530

    finish section5

--- src/pages/Section5Page.vue
+++ src/pages/Section5Page.vue
@@ -8,45 +8,39 @@
 
         <form class="q-gutter-md" @submit.prevent.stop="onSubmit">
           <div class="q-pa-xl">
-            <div class="text-h6 q-px-xl montserrat q-mb-md">Section 5</div>
-            <div v-for="(que, index) in questionsFinal" :key="que.id">
-              <div class="col-12 q-px-xl q-mt-xl q-pt-md q-pb-md">
-                {{ `${index + 1}) ${que.title}` }}
-              </div>
-              <!-- {{ choice }} -->
-              <div class="row">
-                <div class="col-2">
-                  <q-option-group
-                    v-model="answers[index]"
-                    class="q-px-xl"
+            <div class="text-h6 q-px-xl montserrat q-mb-md">
+              Section 5: Brain Test
+            </div>
+
+            <div class="col-12 q-px-xl q-mt-xl q-pt-md q-pb-md">
+              {{ questions.instructions }}
+            </div>
+
+            <div v-for="(que, index) in questions.questions" :key="que.id">
+              <div
+                class="col-12 q-px-xl q-mt-xl q-pt-md q-pb-md"
+                :class="{
+                  'bg-red-1': invalidAnswers.includes(que.id),
+                }"
+              >
+                <p>{{ `${index + 1}) ${que.label}` }}</p>
+
+                <q-option-group
+                  v-model="answers[index]"
+                  :options="que.options"
+                  type="radio"
+                  color="primary"
+                />
+                <!-- <template v-for="opt in que.options" :key="opt.val">
+                  <q-radio
+                    v-model=""
+                    :val="opt.val"
+                    :label="opt.label"
                     color="primary"
-                    :options="options"
-                    type="radio"
                   />
-                </div>
+                  <br />
+                </template> -->
               </div>
-              <!-- <div
-                v-for="choice in que.choices"
-                :key="choice.id"
-                class="q-px-xl"
-              > -->
-              <!-- <q-radio
-                  name="shape"
-                  v-model="answers[index][index2]"
-                  :val="choice.answer"
-                  :label="choice.answer"
-                  color="primary"
-                /> -->
-              <!-- <q-checkbox
-                  dark
-                  v-model="answers[index][index2]"
-                  :label="que.choices[index2].answer"
-                  color="primary"
-                  :true-value="true"
-                  :false-value="false"
-                  keep-color
-                /> -->
-              <!-- </div> -->
             </div>
           </div>
           <div class="col-12">
@@ -63,109 +57,95 @@
         </form>
       </div>
     </div>
-    <div id="content">
-      <!-- <section style="margin-bottom: 200px">
-                <img src="https://qs-training-resources.s3.amazonaws.com/training_practice_tests/banner.jpg" alt="">
-                <h1 style="text-align: center;">Career Suitability Assessment</h1>
-                <div class="content">
-                    <div class="applicant-details">
-                        <div class="details">
-                            <p>Applicant ID</p>
-                            <p>Name</p>
-                            <p>Email</p>
-                            <p>Phone</p>
-                            <p>Class and Section</p>
-                            <p>Institution</p>
-                        </div>
-                        <div class="info">
-                            <p>: ${data.applicantID}</p>
-                            <p>: ${data.name}</p>
-                            <p>: ${data.email}</p>
-                            <p>: ${data.phone}</p>
-                            <p>: ${data.classSection}</p>
-                            <p>: ${data.institutionName}</p>
-                        </div>
-                    </div>
-                    <br>
-                    <p style="margin-bottom: 10px;"><strong>Dear Student ,</strong> </p>
-                    <p style="margin-bottom: 10px;">Innovation Nest (INN) was founded by like-minded group of senior management
-                        professionals to support individuals
-                        and organization through Training, Coaching, Consulting and Development. Since our inception, we have
-                        been
-                        totally focusing on building competencies creatively with support of technology and tools.</p>
-                    <p>MentoNest is an initiative of Innovation Nest. We are the best-in-class career
-                        management organisation. Since our
-                        inception, we have been involved in designing and developing solutions and providing mentoring for
-                        students
-                        and
-                        young professionals.</p>
-                    <br>
-                    <ul style="margin-left: 12px;">
-                        <li><strong style="padding-left: 25px;">WHAT</strong></li>
-                        <p style="padding-left: 10px;">
-                            Teaching, medicine, mechanics, interior designing, web development, entrepreneurship, hospitality,
-                            and
-                            journalism are some of the good career options to choose. Choose a career that suits your skills and
-                            interests you. Because you can be successful in the chosen field only if you have an interest in it
-                            with
-                            our
-                            support.
-                        </p>
-                        <br>
-                        <li><strong style="padding-left: 25px;">WHY</strong></li>
-                        <p style="padding-left: 10px;">
-                            Achieving defined career goals often requires developing strategies and an action plan. Regardless
-                            of
-                            your
-                            field or career aspirations, you can plan and take actionable steps to manage your career
-                            effectively.
-                            Understanding the importance and benefits of career management can help you advance in your career.
-                        </p>
-                        <br>
-                        <li><strong style="padding-left: 25px;">HOW</strong></li>
-                        <p style="padding-left: 10px;">
-                            Adequate skill resourcing is essential, especially during organizational or industrial changes. By
-                            managing
-                            your career effectively, you can evaluate your skills and qualifications and recognize other career
-                            paths
-                            that fit you. Career management helps you become resourceful and advance to roles that best suit
-                            your
-                            skill
-                            sets and meet your aspirations.
-
-                        </p>
-                        <br>
-                        <li><strong style="padding-left: 25px;">WHEN</strong></li>
-                        <p style="padding-left: 10px;">
-                            Total years of working is between 35 years. Setting specific and concise career goals is essential
-                            to
-                            monitor your career development and progress toward achieving the goals. You can create a realistic
-                            and
-                            measurable career goal and steadily take actionable steps to achieve it.
-                        </p>
-                    </ul>
-                </div>
-                <footer>
-                    <p> &#169 innovationnest 2023 </p>
-                    <p>support@innovationnest.in</p>
-                    <p>Powered by AFFCOR</p>
-                </footer>
-            </section> -->
-    </div>
   </q-page>
 </template>
 
 <script>
 import { ref } from 'vue'
+// import { useRouter } from 'vue-router'
+
+import questions from '../assets/sectionFive.json'
+
+import { useAnswerStore } from 'src/stores/answer'
+
 // import { jsPDF } from "jspdf";
-import questions from '../assets/questions.json'
-import sectionOneJson from '../assets/sectionOne.json'
 // import pdfService from "../services/pdfGeneration";
 // import reportTemplate from "../assets/reportTemplate";
 // import indexHtml from "../services/index.html";
-let questionsFinal = questions[4].questions
-let currLength = 0
+
 export default {
+  name: 'Section5Page',
+
+  setup() {
+    // const router = useRouter()
+
+    const answers = ref([])
+    answers.value = Array(questions.questions.length).fill('')
+
+    const invalidAnswers = ref([])
+
+    const validateAnswers = () => {
+      invalidAnswers.value = []
+
+      const isAllAnswered = answers.value.every(a => a !== '')
+
+      if (!isAllAnswered) {
+        answers.value.forEach((a, i) => {
+          if (a === '') {
+            invalidAnswers.value.push(questions.questions[i].id)
+          }
+        })
+
+        alert('Please answer all questions')
+        return false
+      }
+
+      return true
+    }
+
+    const saveResult = () => {
+      if (!validateAnswers()) return
+
+      const answersWithQId = {}
+
+      questions.questions.forEach((q, i) => {
+        answersWithQId[`q${q.id}`] = answers.value[i]
+      })
+
+      const sumA = answers.value.filter(a => a === 'A').length
+      const sumB = answers.value.filter(a => a === 'B').length
+
+      const verdict =
+        sumA > sumB ? 'left-brain dominate' : 'right-brain dominate'
+
+      const section5result = {
+        answers: answersWithQId,
+        sumA,
+        sumB,
+        verdict,
+      }
+
+      useAnswerStore().section5 = section5result
+    }
+
+    const onSubmit = () => {
+      saveResult()
+      // router.push('/all-done')
+      alert('All done!')
+    }
+
+    return {
+      questions,
+      answers,
+      invalidAnswers,
+      onSubmit,
+    }
+  },
+
+  /*
+   // old setup
+
+
   setup() {
     return {
       questionsFinal,
@@ -286,5 +266,6 @@ export default {
       },
     }
   },
+  */
 }
 </script>
