e5c3f78e0869b71ade2df1e681fa4ea4fa2855d5
Krushn Dayshmookh <krushn.dayshmookh.ext@siemens.com>
2024-03-29 18:06:06 +0530

    try pdf generation on frontend

--- src/pages/AllDonePage.vue
+++ src/pages/AllDonePage.vue
@@ -22,6 +22,9 @@ import { useAnswerStore } from 'src/stores/answer'
 
 import { api } from 'src/boot/axios'
 
+import { jsPDF } from 'jspdf'
+import html2canvas from 'html2canvas'
+
 export default {
   name: 'Section3Page',
 
@@ -32,7 +35,112 @@ export default {
 
     const status = ref('pending')
 
-    const sendData = () => {
+    const sendData = async (data, pdfBuffer) => {
+      $q.loading.show()
+
+      const formData = new FormData()
+      formData.append('data', JSON.stringify(data))
+      formData.append('pdf', new Blob([pdfBuffer], { type: 'application/pdf' }))
+
+      try {
+        await api.post('/submit', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        $q.notify({
+          message: 'Request submitted successfully',
+          color: 'positive',
+        })
+        status.value = 'success'
+      } catch {
+        $q.notify({
+          message: 'Failed to submit request',
+          color: 'negative',
+        })
+
+        status.value = 'failed'
+      } finally {
+        $q.loading.hide()
+      }
+    }
+
+    const generatePDF = async data => {
+      const content = `<html>
+          <head>
+            <style>
+              body {
+                font-family: 'Poppins', sans-serif;
+              }
+              h6 {
+                font-size: 14px;
+                font-weight: 600;
+                margin-top: 10px;
+                margin-bottom: 5px;
+                color: red;
+              }
+              p {
+                font-size: 12px;
+                margin-top: 5px;
+                margin-bottom: 5px;
+              }
+              ul {
+                list-style-type: none;
+                padding: 0;
+              }
+              li {
+                font-size: 12px;
+                margin-top: 5px;
+                margin-bottom: 5px;
+              }
+            </style>
+          </head>
+
+          <body>
+            <div style='font-size:11px; background-color: rgb(239 240 240); padding: 05px 15px; width:595pt;'>
+              <h6>Top 3 Career Groups</h6>
+              <ul>
+                ${data.pdfData.top3CareerGroups
+                  .map(group => `<li>${group}</li>`)
+                  .join('')}
+              </ul>
+              <h6>Skills</h6>
+              <ul>
+                ${data.pdfData.skills
+                  .map(skill => `<li>${skill}</li>`)
+                  .join('')}
+              </ul>
+              <h6>Emotional Intelligence</h6>
+              <p>${data.pdfData.emotionalIntelligence}</p>
+              <h6>Personality</h6>
+              <p>${data.pdfData.personality}</p>
+              <h6>Brain Type</h6>
+              <p>${data.pdfData.brainType}</p>
+            </div>
+          </body>
+        </html>
+        `
+
+      console.log(content)
+
+      window.html2canvas = html2canvas
+
+      const doc = new jsPDF({
+        orientation: 'portrait',
+        unit: 'pt',
+        format: 'a4',
+      })
+
+      doc.html(content, {
+        callback: pdf => {
+          // pdf.output('pdfobjectnewwindow')
+          const pdfBuffer = pdf.output('arraybuffer')
+          sendData(data, pdfBuffer)
+        },
+      })
+    }
+
+    const init = async () => {
       const finalResult = {
         personalDetails: answerStore.personalDetails,
         section1: answerStore.section1,
@@ -49,33 +157,11 @@ export default {
         },
       }
 
-      $q.loading.show()
-
-      api
-        .post('/submit', finalResult)
-        .then(() => {
-          $q.notify({
-            message: 'Request submitted successfully',
-            color: 'positive',
-          })
-
-          status.value = 'success'
-        })
-        .catch(() => {
-          $q.notify({
-            message: 'Failed to submit request',
-            color: 'negative',
-          })
-
-          status.value = 'failed'
-        })
-        .finally(() => {
-          $q.loading.hide()
-        })
+      await generatePDF(finalResult)
     }
 
-    onMounted(() => {
-      sendData()
+    onMounted(async () => {
+      await init()
     })
 
     return {
